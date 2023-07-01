dacc32299624e024ad35d734c022610ba3177b0c
HuizeXing <2804970x@student.gla.ac.uk>
2023-07-01 08:01:25 +0800

    in

---
+++ untitled/src/main/java/test.java
@@ -0,0 +1,8 @@
+public class test {
+        /* Here is the explanation for the code above:
+1. Sort the candidates;
+2. Use HashSet to avoid duplicate result;
+3. Use start to avoid duplicate result;
+4. Use target < 0 to avoid unnecessary computation. */
+    
+}
